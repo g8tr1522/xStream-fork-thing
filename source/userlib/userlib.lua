9d284156ea037ab56f6f23377dd383c29b93ecc3
William Prophet <g8tr1522@gmail.com>
2018-01-08 17:46:43 -0500

    removed LAM and Phrases packages from userlib

--- source/userlib/userlib.lua
+++ source/userlib/userlib.lua
@@ -1,27 +1,35 @@
--- require all user modules/libraries as submodules here
+-- require all user libraries/submodules as lua packages here
+-- in main.lua, there should be a constant `_userlibroot` with value "source/userlib/"
 
 userlib = {}
 
--- in main.lua, there should be a constant `_userlibroot` with value "source/userlib/"
 
-_mainroot = ''	-- avoids errors from testing for existence of `_mainroot` in Phrases and LAM
---local _old_mainroot = ''
---if _mainroot then _old_mainroot = _mainroot end  -- stash old mainroot and reload it at the end of this file
---_mainroot = _mainroot or ''
 
+
+--=============================================================================
+-- require user packages here
+--
+-- You 'install' your package with 3 steps:
+--  1) Add the package folder in the "source/userlib/." folder
+--	2) by requiring it's main module/file in this file
+--	3) by adding appropriate members to the `props_table` table in "source/xStreamModel.lua"
+-- see the example package below
+--
+--
 
 -- example package
 _eproot = _userlibroot .. "example_package/"
 userlib.example_package = require(_eproot .. "example_package")
+	-- now, for step 3, go to xStreamModel.lua, and see lines 318-320
+	-- replicate what you see there for other packages.
 
--- LuaArrayMethods 
-_lamroot = _userlibroot .. "LuaArrayMethods/"
-userlib.LuaArrayMethods = require(_lamroot .. 'lam')
 
--- Phrases
-_phrasesroot = _userlibroot .. "Phrases/"
-userlib.Phrases = require(_phrasesroot .. 'Phrases')
 
+
+--=============================================================================
+-- print some info which alerts the user if packages were loaded successfully
+--
+--
 
 print()
 print("~~~ Loading userlib module...")
@@ -29,9 +37,9 @@ print("  ~ userlib.lua folder : ".._userlibroot)
 print("~~~ Locations: ")
 print("  ~ userlib                        ", userlib)
 print("  ~ userlib.example_package        ", userlib.example_package)
---print("  ~ userlib.example_package.foo ", userlib.example_package.foo)
-print("  ~ userlib.LuaArrayMethods        ", userlib.LuaArrayMethods)
-print("  ~ userlib.Phrases                ", userlib.Phrases)
 print('\n')
+
+
+
 
 return userlib
